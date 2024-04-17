b55fea700966244c3ec060907b9b2bda42242b78
RaulWalls <raul.walls18@gmail.com>
2024-04-17 16:11:50 -0600

    update

---
+++ blife-app/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Header.css';
+
+function Header(){
+    return(
+        <div className="container-nav">
+            <div className='column1'>
+            column1
+            </div>
+            <div className='column2'>
+            column1
+            </div>
+            <div className='column3'>
+            column1
+            </div>
+        </div>
+    );
+}
+
+export default Header;
